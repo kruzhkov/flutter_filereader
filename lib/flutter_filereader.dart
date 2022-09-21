c0d23c599c7dae8868782659475fc19b47f12aa7
Sergey Kruzhkov <s.kruzhkov@gmail.com>
2022-09-21 09:22:49 +0300

    Update flutter_filereader.dart
    change message to Russian

--- lib/flutter_filereader.dart
+++ lib/flutter_filereader.dart
@@ -73,20 +73,20 @@ class _FileReaderViewState extends State<FileReaderView> {
         return _loadingWidget();
       }
     } else {
-      return Center(child: Text("不支持的平台"));
+      return Center(child: Text("Просмотр не доступен"));
     }
   }
 
   Widget _unSupportFile() {
     return widget.unSupportFileWidget ??
         Center(
-          child: Text("不支持打开${_fileType(filePath!)}类型的文件"),
+          child: Text("Тип файла ${_fileType(filePath!)} не поддерживается"),
         );
   }
 
   Widget _fileNotFoundFile() {
     return Center(
-      child: Text("文件不存在"),
+      child: Text("Файл не найден"),
     );
   }
 
@@ -94,7 +94,7 @@ class _FileReaderViewState extends State<FileReaderView> {
     //最有可能是abi的问题
     //还有可能第一次下载成功,但是加载不成功
     return Center(
-      child: Text("引擎加载失败,请退出重试"),
+      child: Text("Ошибка загрузки, попробуйте еще раз"),
     );
   }
 
